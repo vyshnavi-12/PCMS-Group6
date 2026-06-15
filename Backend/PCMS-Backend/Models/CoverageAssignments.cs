00ae685261a37854fd96b5f47ed936a8bb685cc7
Saikrishna <spasupul11@gitam.in>
2026-06-15 18:30:59 +0530

    Models

---
+++ Backend/PCMS-Backend/Models/CoverageAssignments.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace PCMS_Backend.Models;
+
+public class CoverageAssignments
+{
+    [Key]
+    public int CoverageAssignmentId { get; set; }
+
+    public int CoverageScheduleId { get; set; }
+
+    public DateOnly CoverageDate { get; set; }
+
+    public int SpecialtyId { get; set; }
+
+    public int PhysicianId { get; set; }
+
+    public string ShiftType { get; set; } = default!;
+
+    public string AssignmentStatus { get; set; } = default!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public CoverageSchedule CoverageSchedule { get; set; }
+
+    public Specialty Specialty { get; set; }
+
+    public Physicians Physicians { get; set; }
+
+    public ICollection<CoverageGapAlerts>? CoverageGapAlerts { get; set; }
+
+    public ICollection<SwapRequests>? SwapRequests { get; set; }
+}
---
+++ Backend/PCMS-Backend/Models/CoverageGapAlerts.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace PCMS_Backend.Models;
+
+
+public class CoverageGapAlerts
+{
+    [Key]
+    public int CoverageGapAlertId { get; set; }
+
+    public int CoverageAssignmentId { get; set; }
+
+    public int? SuggestedReplacementPhysicianId { get; set; }
+
+    public string? AlertStatus { get; set; }
+
+    public string? AlertReason { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? ResolvedAt { get; set; }
+
+    public CoverageAssignments CoverageAssignment { get; set; } = default!;
+
+    public Physicians? SuggestedReplacementPhysician { get; set; }
+}

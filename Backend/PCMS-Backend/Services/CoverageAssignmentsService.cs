c89455f0ad456e823194a8b1d25f8db0a841acd9
Saikrishna <spasupul11@gitam.in>
2026-06-29 12:06:44 +0530

    Email

--- Backend/PCMS-Backend/Services/CoverageAssignmentsService.cs
+++ Backend/PCMS-Backend/Services/CoverageAssignmentsService.cs
@@ -144,12 +144,12 @@ public class CoverageAssignmentsService : ICoverageAssignmentsService
 
         var replacements =
             recommendations
-                .Select((r,index) => new ReplacementPhysicianDto
+                .Select((r, index) => new ReplacementPhysicianDto
                 {
                     PhysicianId = r.PhysicianId,
                     PhysicianName = r.PhysicianName,
-                    IsRecommended=index==0
-                    
+                    IsRecommended = index == 0
+
                 })
                 .ToList();
 
@@ -224,7 +224,7 @@ public class CoverageAssignmentsService : ICoverageAssignmentsService
 
     public async Task<Result<IReadOnlyList<UnavailableRequestsPerSpecialtyDto>>> GetUnavailableRequestsPerSpecialtyAsync()
     {
-        var data =  await _coverageAssignmentsRepo.GetUnavailableRequestsPerSpecialtyAsync();
+        var data = await _coverageAssignmentsRepo.GetUnavailableRequestsPerSpecialtyAsync();
         if (data == null) return Result<IReadOnlyList<UnavailableRequestsPerSpecialtyDto>>.ServerError("Failed to fetch data");
         return Result<IReadOnlyList<UnavailableRequestsPerSpecialtyDto>>.Ok(data);
     }
